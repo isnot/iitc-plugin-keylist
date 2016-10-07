74d8df3144995e43dfc98d6767f62cd253c347ec
ISHIDA Naoto <naoto@isnot.jp>
2016-10-07 11:42:15 +0900

    subdomain issue

--- plugins/keys-list.user.js
+++ plugins/keys-list.user.js
@@ -8,14 +8,14 @@
 // @updateURL      https://github.com/isnot/iitc-plugin-keys-list/raw/master/plugins/keys-list.user.js
 // @downloadURL    https://github.com/isnot/iitc-plugin-keys-list/raw/master/plugins/keys-list.user.js
 // @description    [iitc-plugins] Export the manually entered key counts from the 'keys' plugin.
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
-// @include        https://www.ingress.com/mission/*
-// @include        http://www.ingress.com/mission/*
-// @match          https://www.ingress.com/mission/*
-// @match          http://www.ingress.com/mission/*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
 // @grant          none
 // ==/UserScript==
 
